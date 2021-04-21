0c01c36fdf456b6fc12983ce56c033ab4aeab1d9
Leskhan.Karatayev2 <leskhan.karatayev2@homecredit.kz>
2021-04-21 12:39:37 +0600

    refactoring

--- pages/detail.vue
+++ pages/detail.vue
@@ -4,21 +4,88 @@
       <div class="flex flex-col max-w-md bg-white px-8 py-6 rounded-xl space-y-5">
         <h3 class="font-serif font-bold text-gray-900 text-xl">Омлет с овощами</h3>
         <img class="w-full rounded-md" src="https://img.pizza/150/150" alt="pizza" />
-        <p class="font-bold text-left">Description</p>
-        <p class="leading-relaxed">Lorem  ipsum asdsad asd asd </p>
-        <p class="">Calorie of food(kcal): 299</p>
-        <button class="px-24 py-4 bg-regular-yellow rounded-md text-white text-sm focus:border-transparent">Watch video</button>
+        <div class='row'>
+          <div class='column'>
+            <div class='first-column'>
+              <star-icon class="pr-1"/>
+              <star-icon class="pr-1"/>
+              <star-icon class="pr-1"/>
+              <star-icon class="pr-1"/>
+              <star-icon class="pr-1"/>
+            </div>
+          </div>
+          <div class='column'>
+            <div class='second-column'>
+              <h3>Чайла</h3>
+            </div>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='column'>
+            <div class='first-column text-xs'>
+              <rest-button
+                title="Share"
+                type="primary"
+                :svgIcon="'shareIcon'"
+              />
+            </div>
+          </div>
+          <div class='column'>
+            <div class='second-column text-xs' >
+              <rest-button
+                title="20 min"
+                type="primary"
+                :svgIcon="'timerIcon'"
+              />
+            </div>
+          </div>
+        </div>
+        <p class="font-bold text-left font-rest-black text-base">Description</p>
+        <p class="leading-relaxed text-sm font-rest-black">Lorem  ipsum asdsad asd asd </p>
+        <p class="font-rest-black text-xs"><span class="font-bold">Calorie of food(kcal):</span> 299</p>
+        <rest-button
+          class="px-24 py-4 bg-regular-yellow rounded-md text-white focus:border-transparent"
+          title="Watch video"
+          type="primary"
+          :svgIcon="'videoIcon'"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import StarIcon from "../components/icons/starIcon";
 export default {
-  name: "detail"
+  name: "detail",
+  components: {StarIcon},
 }
 </script>
 
 <style scoped>
 
+.row {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+}
+
+.column {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+
+.first-column {
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.second-column {
+  text-align: right;
+  height: 100%;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -21,21 +21,33 @@
     <div class="holder mx-auto w-10/12 grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 rounded-md">
       <div class="each mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative">
         <h1 class="font-bold">Омлет с овощами</h1>
-        <img class="w-full" src="https://i.ytimg.com/vi/qew27BNl7io/maxresdefault.jpg" alt="" />
-        <nuxt-link to="/detail" class="badge absolute top-0 right-0 m-1 text-regular-yellow  px-2 text-xs font-bold rounded">Состав</nuxt-link>
-        <div class="info-box text-xs flex p-1 font-semibold text-regular-yellow">
-          <span class="mr-1 p-1 px-2 font-bold">*</span>
-          <span class="mr-1 p-1 px-2 font-bold text-gray-500">(124 ratings) Завтрак</span>
+<!--        <img class="w-full" src="https://i.ytimg.com/vi/qew27BNl7io/maxresdefault.jpg" alt="" />-->
+        <iframe class="w-full"
+                src="https://www.youtube.com/watch?v=L3j_BLWqJSU?autoplay=1&mute=1">
+        </iframe>
+        <nuxt-link to="/detail" class="badge absolute top-0 right-0 m-1 text-rest-yellow  px-2 text-xs rounded">Состав</nuxt-link>
+        <div class="info-box text-xs flex p-1 font-semibold text-rest-yellow">
+          <span class="p-1"><star-icon/></span>
+          <span class="mr-1 p-1 font-bold text-gray-500">(124 ratings) Завтрак</span>
         </div>
         <div class='row'>
           <div class='column'>
-            <div class='first-column'>
-              <p>icon</p>
+            <div class='first-column text-xs'>
+              <rest-button
+                title="20 min"
+                type="primary"
+                :svgIcon="'timerIcon'"
+              />
             </div>
           </div>
           <div class='column'>
-            <div class='second-column'>
-              <h3>Share</h3>
+            <div class='second-column text-xs' >
+              <rest-button
+                class="ml-11"
+                title="Share"
+                type="primary"
+                :svgIcon="'shareIcon'"
+              />
             </div>
           </div>
         </div>
@@ -49,7 +61,7 @@
       </div>
       <div class='column'>
         <div class='second-column'>
-          <h6 class="text-regular-yellow">Посмотреть все</h6>
+          <h6 class="text-rest-yellow">Посмотреть все</h6>
         </div>
       </div>
     </div>
@@ -58,10 +70,11 @@
         <img alt="mountain" class="w-45 rounded-md border-2 border-gray-300" src="https://img.pizza/100/100" />
         <div  class="flex flex-col ml-5">
           <h4  class="text-xl font-semibold mb-2">Пицца Маргарита</h4>
-          <p  class="text-gray-800 mt-2">Завтрак ~ Пицца</p>
-          <div class="flex mt-5">
-            <span class="mr-1 p-1 px-2 font-bold text-regular-yellow">*</span>
-            <span class="mr-1 p-1 px-2 font-bold text-gray-500">(124 ratings) Завтрак</span>
+          <p  class="text-gray-800 mb-2">Завтрак ~ Пицца</p>
+          <div class="flex">
+            <star-icon class="mr-2"/>
+            <span class="text-rest-yellow text-xs">4.9</span>
+            <span class="mr-1 px-2 font-bold text-gray-500 text-xs">(124 ratings)</span>
           </div>
         </div>
       </div>
@@ -70,11 +83,12 @@
       <div id="header" class="flex">
         <img alt="mountain" class="w-45 rounded-md border-2 border-gray-300" src="https://img.pizza/100/100" />
         <div id="body" class="flex flex-col ml-5">
-          <h4 id="name" class="text-xl font-semibold mb-2">Пицца Маргарита</h4>
-          <p id="job" class="text-gray-800 mt-2">Завтрак ~ Пицца</p>
-          <div class="flex mt-5">
-            <span class="mr-1 p-1 px-2 font-bold text-regular-yellow">*</span>
-            <span class="mr-1 p-1 px-2 font-bold text-gray-500">(124 ratings) Завтрак</span>
+          <h4 class="text-xl font-semibold mb-2">Пицца Маргарита</h4>
+          <p  class="text-gray-800 mb-2">Завтрак ~ Пицца</p>
+          <div class="flex">
+            <star-icon class="mr-2"/>
+            <span class="text-rest-yellow text-xs">4.9</span>
+            <span class="mr-1 px-2 font-bold text-gray-500 text-xs">(124 ratings)</span>
           </div>
         </div>
       </div>
@@ -84,10 +98,11 @@
         <img alt="mountain" class="w-45 rounded-md border-2 border-gray-300" src="https://img.pizza/100/100" />
         <div  class="flex flex-col ml-5">
           <h4  class="text-xl font-semibold mb-2">Пицца Маргарита</h4>
-          <p  class="text-gray-800 mt-2">Завтрак ~ Пицца</p>
-          <div class="flex mt-5">
-            <span class="mr-1 p-1 px-2 font-bold text-regular-yellow">*</span>
-            <span class="mr-1 p-1 px-2 font-bold text-gray-500">(124 ratings) Завтрак</span>
+          <p  class="text-gray-800 mb-2">Завтрак ~ Пицца</p>
+          <div class="flex">
+            <star-icon class="mr-2"/>
+            <span class="text-rest-yellow text-xs">4.9</span>
+            <span class="mr-1 px-2 font-bold text-gray-500 text-xs">(124 ratings)</span>
           </div>
         </div>
       </div>
@@ -96,7 +111,10 @@
 </template>
 
 <script>
-export default {}
+import StarIcon from "../components/icons/starIcon";
+export default {
+  components: {StarIcon}
+}
 </script>
 
 <style>
