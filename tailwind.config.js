8677417f205da967bd705e1d44a86b321bebcb33
Leskhan.Karatayev2 <leskhan.karatayev2@homecredit.kz>
2021-04-21 16:31:28 +0600

    added new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
     extend: {
       colors: {
         'rest-yellow': '#f0b62b',
-        'rest-black':'#b8b9b9'
+        'rest-black':'#b8b9b9',
+        'rest-blacker':'#414141'
       }
     }
   }
